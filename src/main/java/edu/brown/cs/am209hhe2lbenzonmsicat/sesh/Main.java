b15b28092ba532df79b010c7df8deabc4ef55716
Hannah He <HE23@Hannah-Hes-MacBook-Air.local>
2017-04-12 16:52:37 -0400

    Set up Main and GuiManager

--- src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Main.java
+++ src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Main.java
@@ -1,6 +1,33 @@
 package edu.brown.cs.am209hhe2lbenzonmsicat.sesh;
 
-public class Main {
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import freemarker.template.Configuration;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import spark.ExceptionHandler;
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
+
+/**
+ * Main class.
+ *
+ * @author HE23
+ *
+ */
+public final class Main {
+
+  /**
+   * Main method.
+   *
+   * @param args
+   *          - program arguments.
+   */
   public static void main(String[] args) {
     new Main(args).run();
   }
@@ -12,7 +39,50 @@ public class Main {
   }
 
   private void run() {
+    OptionParser parser = new OptionParser();
+    parser.accepts("port").withRequiredArg().ofType(Integer.class)
+        .defaultsTo(Constants.DEFAULT_PORT);
+    OptionSet options = parser.parse(args);
+    runSparkServer((int) options.valueOf("port"));
+  }
 
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File("src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable use %s for template loading.%n",
+          templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+
+  private void runSparkServer(int port) {
+    Spark.port(port);
+    Spark.externalStaticFileLocation("src/main/resources/static");
+    Spark.exception(Exception.class, new ExceptionPrinter());
+    new GuiManager(createEngine());
+  }
+
+  /**
+   * Display an error page when an exception occurs in the server.
+   *
+   * @author jj
+   */
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(500);
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
+    }
   }
 
 }
