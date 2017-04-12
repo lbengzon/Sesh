fed5730efdd6e23c5304f52bbc9b49376f2d1777
Leandro <leandro@Leandros-MacBook-Pro-3.local>
2017-04-12 16:29:54 -0400

    setup

--- src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Main.java
+++ src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Main.java
@@ -1,5 +1,21 @@
 package edu.brown.cs.am209hhe2lbenzonmsicat.sesh;
 
-public class Main {
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public class Main {
+	public static void main(String[] args) {
+	    new Main(args).run();
+	  }
+
+	  private String[] args;
+
+	  private Main(String[] args) {
+	    this.args = args;
+	  }
+
+	  private void run() {
+
+	  }
 }
