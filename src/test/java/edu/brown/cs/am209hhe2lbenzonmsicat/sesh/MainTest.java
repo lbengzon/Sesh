888167fd7359bd35da7bfdabeb27e266ecbddf69
Hannah He <HE23@Hannah-Hes-MacBook-Air.local>
2017-04-12 16:34:48 -0400

    Project all set up

--- src/test/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/MainTest.java
+++ src/test/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/MainTest.java
@@ -1,14 +1,12 @@
 package edu.brown.cs.am209hhe2lbenzonmsicat.sesh;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class MainTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+  @Test
+  public void test() {
+    System.out.println("here");
+  }
 
 }
