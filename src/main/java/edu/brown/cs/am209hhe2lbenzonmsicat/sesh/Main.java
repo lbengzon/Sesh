0741baabdd461ca373e3dffe2e14137a0a067ecf
Hannah He <HE23@Hannah-Hes-MacBook-Air.local>
2017-04-12 16:34:48 -0400

    Project all set up

--- src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Main.java
+++ src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Main.java
@@ -1,21 +1,18 @@
 package edu.brown.cs.am209hhe2lbenzonmsicat.sesh;
 
-
-import joptsimple.OptionParser;
-import joptsimple.OptionSet;
-
 public class Main {
-	public static void main(String[] args) {
-	    new Main(args).run();
-	  }
+  public static void main(String[] args) {
+    new Main(args).run();
+  }
 
-	  private String[] args;
+  private String[] args;
 
-	  private Main(String[] args) {
-	    this.args = args;
-	  }
+  private Main(String[] args) {
+    this.args = args;
+  }
 
-	  private void run() {
+  private void run() {
 
-	  }
+  }
+
 }
