ad030bff2925f27d90e1659c2d998c0c5589069f
Leandro Bengzon <leandro_bengzon@brown.edu>
2017-04-12 16:29:54 -0400

    setup

---
+++ src/test/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/MainTest.java
@@ -0,0 +1,14 @@
+package edu.brown.cs.am209hhe2lbenzonmsicat.sesh;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
