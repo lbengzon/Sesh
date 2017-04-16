16fb0556efba9f5aac46107123dcdd60882c7c5c
Hannah He <HE23@Hannah-Hes-MacBook-Air.local>
2017-04-16 19:24:56 -0400

    Adding front end

--- src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/GuiManager.java
+++ src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/GuiManager.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import com.google.common.collect.ImmutableMap;
 
 import spark.ModelAndView;
-import spark.QueryParamsMap;
 import spark.Request;
 import spark.Response;
 import spark.Spark;
@@ -14,39 +13,67 @@ import spark.template.freemarker.FreeMarkerEngine;
 
 /**
  * Gui Manager class.
+ *
  * @author HE23
+ *
  */
 public class GuiManager {
-  SpotifyCommunicator comm = new SpotifyCommunicator();
 
   /**
    * Default constructor.
+   *
    * @param freeMarkerEngine
    *          - freemarker engine
    */
   public GuiManager(FreeMarkerEngine freeMarkerEngine) {
     installRoutes(freeMarkerEngine);
-    comm.createAuthorizeURL();
-
   }
 
   private void installRoutes(FreeMarkerEngine fme) {
-    // install spark routes here
-    Spark.get("/spotifycallback", new MapsHandler(), fme);
-
+    Spark.get("/sesh", new FrontHandler(), fme);
+    Spark.get("/create", new CreateHandler(), fme);
+    Spark.get("/join", new JoinHandler(), fme);
   }
 
-  private class MapsHandler implements TemplateViewRoute {
+  /**
+   * Handles request to front page (join or create a sesh).
+   *
+   * @author HE23
+   *
+   */
+  private static class FrontHandler implements TemplateViewRoute {
     @Override
     public ModelAndView handle(Request req, Response res) {
-      QueryParamsMap qm = req.queryMap();
+      Map<String, Object> variables = ImmutableMap.of("title", "Sesh");
+      return new ModelAndView(variables, "createJoin.ftl");
+    }
+  }
 
-      String code = qm.value("code");
-      System.out.println(code);
-      comm.getAccessToken(code);
+  /**
+   * Handles request to create a sesh page.
+   *
+   * @author HE23
+   *
+   */
+  private static class CreateHandler implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      Map<String, Object> variables = ImmutableMap.of("title", "Create a Sesh");
+      return new ModelAndView(variables, "create.ftl");
+    }
+  }
 
-      Map<String, Object> variables = ImmutableMap.of("title", "Maps");
-      return new ModelAndView(variables, "home.ftl");
+  /**
+   * Handles request to join a sesh page.
+   *
+   * @author HE23
+   *
+   */
+  private static class JoinHandler implements TemplateViewRoute {
+    @Override
+    public ModelAndView handle(Request req, Response res) {
+      Map<String, Object> variables = ImmutableMap.of("title", "Join a Sesh");
+      return new ModelAndView(variables, "join.ftl");
     }
   }
 
