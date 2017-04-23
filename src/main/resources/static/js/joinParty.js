b620021d05c77c6126f8dc446ab7e49462f341e6
Hannah He <HE23@Hannah-Hes-MacBook-Air.local>
2017-04-23 17:29:49 -0400

    Able to click on search result item to request it, displays on request list

--- src/main/resources/static/js/joinParty.js
+++ src/main/resources/static/js/joinParty.js
@@ -1,4 +1,97 @@
+function hoverOn(x) {
+	x.className = 'selected';
+}
+
+function hoverOff(x) {
+	x.classList.remove('selected');
+}
+
+function showPlaylist($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper) {
+	$playlistGuest.addClass("active");
+	$requestsGuest.removeClass("active");
+	$searchGuest.removeClass("active");
+	$optionsGuest.removeClass("active");
+	$tabContentRequestGuest.hide();
+	$tabContentOptionsGuest.hide();
+	$tabContentSearchGuest.hide();
+	$tabContentPlaylistGuest.show();
+	$requestTitle.hide();
+	$playlistTitle.show();
+	$listWrapper.height("50%");
+}
+
+function showRequests($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper) {
+	$playlistGuest.removeClass("active");
+	$requestsGuest.addClass("active");
+	$searchGuest.removeClass("active");
+	$optionsGuest.removeClass("active");
+	$tabContentRequestGuest.show();
+	$tabContentOptionsGuest.hide();
+	$tabContentSearchGuest.hide();
+	$tabContentPlaylistGuest.hide();
+	$playlistTitle.hide();
+	$requestTitle.show();
+	$listWrapper.height("50%");
+}
+
+function showSearch($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper) {
+	$playlistGuest.removeClass("active");
+	$requestsGuest.removeClass("active");
+	$searchGuest.addClass("active");
+	$optionsGuest.removeClass("active");
+	$tabContentRequestGuest.hide();
+	$tabContentOptionsGuest.hide();
+	$tabContentSearchGuest.show();
+	$tabContentPlaylistGuest.hide();
+	$playlistTitle.hide();
+	$requestTitle.hide();
+	$listWrapper.height("56%");
+}
+
+function showOptions($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper) {
+	$playlistGuest.removeClass("active");
+	$requestsGuest.removeClass("active");
+	$searchGuest.removeClass("active");
+	$optionsGuest.addClass("active");
+	$tabContentRequestGuest.hide();
+	$tabContentOptionsGuest.show();
+	$tabContentSearchGuest.hide();
+	$tabContentPlaylistGuest.hide();
+	$playlistTitle.hide();
+	$requestTitle.hide();
+	$listWrapper.height("56%");
+}
+
+
 $(document).ready(() => {
+
+	const $userInput = $(".search");
+	const $results = $(".searchResults");
+	const $requests = $(".tabContentRequestGuest ul");
+
+	$userInput.keyup(function() {
+		const postParameters = {userInput: $userInput.val()};
+		$.post("/search", postParameters, responseJSON => {
+			const responseObject = JSON.parse(responseJSON);
+			const suggestions = responseObject.results;
+
+			$results.empty();
+
+			for (var i = 0; i < suggestions.length; i++) {
+				$results.append("<li onmouseover=\"hoverOn(this)\"" + 
+					"onmouseout=\"hoverOff(this)\">" + suggestions[i] + "</li>");
+			}
+		});
+	});
+
+	$results.on("click", event => {
+		$listItems = $('li');
+		$selected = $listItems.filter('.selected');
+		//append to request list
+		$requests.append("<li><div id=\"songdiv\">" + $selected.text() + "<div id=\"vote\"> <button type=\"button\" class=\"voteButton\"> <i class=\"material-icons\">thumb_up</i></button> <button type=\"button\" class=\"voteButton\"> <i class=\"material-icons\">thumb_down</i> </button> </div> </div> </li>")
+		//send post request to create request object and add to db
+		showRequests($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper);
+	});
 
 	//guest tab content
 	const $tabContentRequestGuest = $(".tabContentRequestGuest");
@@ -24,59 +117,19 @@ $(document).ready(() => {
 
 
 	$playlistGuest.click(function() {
-		$playlistGuest.addClass("active");
-		$requestsGuest.removeClass("active");
-		$searchGuest.removeClass("active");
-		$optionsGuest.removeClass("active");
-		$tabContentRequestGuest.hide();
-		$tabContentOptionsGuest.hide();
-		$tabContentSearchGuest.hide();
-		$tabContentPlaylistGuest.show();
-		$requestTitle.hide();
-		$playlistTitle.show();
-		$listWrapper.height("50%");
+		showPlaylist($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper);
 	});
 
 	$requestsGuest.click(function() {
-		$playlistGuest.removeClass("active");
-		$requestsGuest.addClass("active");
-		$searchGuest.removeClass("active");
-		$optionsGuest.removeClass("active");
-		$tabContentRequestGuest.show();
-		$tabContentOptionsGuest.hide();
-		$tabContentSearchGuest.hide();
-		$tabContentPlaylistGuest.hide();
-		$playlistTitle.hide();
-		$requestTitle.show();
-		$listWrapper.height("50%");
+		showRequests($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper);
 	});
 
 	$searchGuest.click(function() {
-		$playlistGuest.removeClass("active");
-		$requestsGuest.removeClass("active");
-		$searchGuest.addClass("active");
-		$optionsGuest.removeClass("active");
-		$tabContentRequestGuest.hide();
-		$tabContentOptionsGuest.hide();
-		$tabContentSearchGuest.show();
-		$tabContentPlaylistGuest.hide();
-		$playlistTitle.hide();
-		$requestTitle.hide();
-		$listWrapper.height("56%");
+		showSearch($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper);
 	});
 
-		$optionsGuest.click(function() {
-		$playlistGuest.removeClass("active");
-		$requestsGuest.removeClass("active");
-		$searchGuest.removeClass("active");
-		$optionsGuest.addClass("active");
-		$tabContentRequestGuest.hide();
-		$tabContentOptionsGuest.show();
-		$tabContentSearchGuest.hide();
-		$tabContentPlaylistGuest.hide();
-		$playlistTitle.hide();
-		$requestTitle.hide();
-		$listWrapper.height("56%");
+	$optionsGuest.click(function() {
+		showOptions($playlistGuest, $requestsGuest, $searchGuest, $optionsGuest, $tabContentRequestGuest, $tabContentOptionsGuest, $tabContentSearchGuest, $tabContentPlaylistGuest, $requestTitle, $playlistTitle, $listWrapper);
 	});
 
 
