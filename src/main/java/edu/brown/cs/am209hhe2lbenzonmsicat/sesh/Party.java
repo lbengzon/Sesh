9d06ab3b9dd9075023b89037b237d6cf7b15ea20
Ali Mir <ali.ahmed.mir@gmail.com>
2017-04-24 20:31:37 -0400

    Fix add songs in comm

--- src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Party.java
+++ src/main/java/edu/brown/cs/am209hhe2lbenzonmsicat/sesh/Party.java
@@ -92,7 +92,7 @@ public abstract class Party {
 
   /**
    * Upvote song.
-   * 
+   *
    * @param user
    *          - to upvote
    * @param req
@@ -103,7 +103,7 @@ public abstract class Party {
 
   /**
    * Downvote song.
-   * 
+   *
    * @param user
    *          - to downvote
    * @param req
@@ -114,7 +114,7 @@ public abstract class Party {
 
   /**
    * Approve song.
-   * 
+   *
    * @param req
    *          - request
    * @return boolean if successful
@@ -123,7 +123,7 @@ public abstract class Party {
 
   /**
    * Remove from playlist.
-   * 
+   *
    * @param req
    *          - request
    * @return boolean if successful.
@@ -132,7 +132,7 @@ public abstract class Party {
 
   /**
    * Request song.
-   * 
+   *
    * @param song
    *          - request
    * @param user
@@ -143,7 +143,7 @@ public abstract class Party {
 
   /**
    * Add a guest to party.
-   * 
+   *
    * @param guest
    *          - guest to add
    * @return boolean if successful.
@@ -152,7 +152,7 @@ public abstract class Party {
 
   /**
    * Removes a guest from the party
-   * 
+   *
    * @param guest
    *          -Guest to remove
    * @return boolean if successful
@@ -161,7 +161,7 @@ public abstract class Party {
 
   /**
    * Gets the distance from the party to the coordinate.
-   * 
+   *
    * @param coordinate
    *          The coordinate to get the distance from.
    * @return The distance from the coordinate.
@@ -179,7 +179,7 @@ public abstract class Party {
 
   /**
    * Retrieve party data.
-   * 
+   *
    * @param partyId
    *          - id
    * @param name
@@ -208,7 +208,7 @@ public abstract class Party {
 
   /**
    * Gets the party object with the party id passed in.
-   * 
+   *
    * @param partyId
    *          The id of the party
    * @return The party object representing the party.
@@ -223,7 +223,7 @@ public abstract class Party {
 
   /**
    * Gets the parties within the distance.
-   * 
+   *
    * @param location
    *          The location of the user.
    * @param distance
@@ -248,7 +248,7 @@ public abstract class Party {
 
   /**
    * Create a party and add to db.
-   * 
+   *
    * @param name
    *          - name
    * @param host
@@ -268,15 +268,13 @@ public abstract class Party {
       throw new NullPointerException(
           "ERROR: Trying to create an party from a null id");
     }
-    // TODO: REMOVE THIS COMMENT String newPlaylistId =
-    // Playlist.getNewPlaylistId(host);
-    String newPlaylistId = "fdsa";
+    String newPlaylistId = Playlist.getNewPlaylistId(host);
     return DbHandler.addParty(newPlaylistId, name, location, time, host);
   }
 
   /**
    * Returns the active party of the user if he has any.
-   * 
+   *
    * @param user
    *          The user you want to get the active party of
    * @return The active party of the user or null if there is no active party.
@@ -291,7 +289,7 @@ public abstract class Party {
 
   /**
    * Gets all (active and stopped) parties of a user.
-   * 
+   *
    * @param user
    *          The user you want to get the parties of.
    * @return The parties of a user.
