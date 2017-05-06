7bbe8ab31b8c47c1069a655e78f24f9795950059
Ali Mir <ali.ahmed.mir@gmail.com>
2017-05-06 19:58:56 -0400

    fix

--- src/main/resources/static/js/websockets.js
+++ src/main/resources/static/js/websockets.js
@@ -46,7 +46,7 @@ function setupWebsockets() {
   const $playlist = $("#playlist-list ul");
   const $player = $("#playback");
   // TODO Create the WebSocket connection and assign it to `conn`
-  conn = new WebSocket("wss://https://sesh.cloud/update");
+  conn = new WebSocket("wss://sesh.cloud/update");
 
   conn.onclose = function(){
     setTimeout(setupWebsockets, 1000)
